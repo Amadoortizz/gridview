1d9de3598e13898e336b493db13fff42c8418d36
FlutLab.io <>
2022-05-02 16:16:50 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter App Learning',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> images = [
+    "assets/images/actanac.jpg",
+    "assets/images/defuncion.jpg",
+    "assets/images/info.jpg",
+    "assets/images/logo.jpg",
+    "assets/images/registro-civil-de-matrimonio.jpg",
+    "assets/images/regularizacion.jpg",
+    "assets/images/actanac.jpg",
+    "assets/images/defuncion.jpg",
+    "assets/images/info.jpg",
+    "assets/images/logo.jpg",
+    "assets/images/registro-civil-de-matrimonio.jpg",
+    "assets/images/regularizacion.jpg",
+    "assets/images/actanac.jpg",
+    "assets/images/defuncion.jpg",
+    "assets/images/info.jpg",
+    "assets/images/logo.jpg",
+    "assets/images/registro-civil-de-matrimonio.jpg",
+    "assets/images/regularizacion.jpg",
+    "assets/images/actanac.jpg",
+    "assets/images/defuncion.jpg",
+    "assets/images/info.jpg",
+    "assets/images/logo.jpg",
+    "assets/images/registro-civil-de-matrimonio.jpg",
+    "assets/images/regularizacion.jpg",
+    "assets/images/actanac.jpg",
+    "assets/images/defuncion.jpg",
+    "assets/images/info.jpg",
+    "assets/images/logo.jpg",
+    "assets/images/registro-civil-de-matrimonio.jpg",
+    "assets/images/regularizacion.jpg",
+    "assets/images/actanac.jpg",
+    "assets/images/defuncion.jpg",
+    "assets/images/info.jpg",
+    "assets/images/logo.jpg",
+    "assets/images/registro-civil-de-matrimonio.jpg",
+    "assets/images/regularizacion.jpg",
+    "assets/images/actanac.jpg",
+    "assets/images/defuncion.jpg",
+    "assets/images/info.jpg",
+    "assets/images/logo.jpg",
+    "assets/images/registro-civil-de-matrimonio.jpg",
+    "assets/images/regularizacion.jpg",
+    "assets/images/actanac.jpg",
+    "assets/images/defuncion.jpg",
+    "assets/images/info.jpg",
+    "assets/images/logo.jpg",
+    "assets/images/registro-civil-de-matrimonio.jpg",
+    "assets/images/regularizacion.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter GridView"),
+        ),
+        body: GridView.builder(
+          itemCount: images.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Image.asset(images[index], fit: BoxFit.cover);
+          },
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 10, crossAxisSpacing: 10),
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
